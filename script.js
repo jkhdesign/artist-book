df71ec49d7848ab5fb6800b28a119e301a78a349
Josh Krauth-Harding <josh@rectangle.design>
2020-11-20 13:15:19 -0500

    Next steps notes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function random(max) {
   return Math.floor(Math.random() * Math.floor(max));
 };
 
-function generateColors() {
+function generateComp() {
   for (i = 1; i < 10; i++) {
     let randomColor = color[random(numOfColors)];
     document.getElementById("c" + i).style.backgroundColor = randomColor;
@@ -16,32 +16,41 @@ function generateColors() {
 window.addEventListener('keydown', function(e) {
   if (e.keyCode == 38 && numOfColors < 9) {
     numOfColors++;
-    generateColors();
+    generateComp();
   }
 });
 
 window.addEventListener('keydown', function(e) {
   if (e.keyCode == 40 && numOfColors > 2) {
     numOfColors--;
-    generateColors();
+    generateComp();
+  }
+});
+
+window.addEventListener('keydown', function(e) {
+  if (e.keyCode == 80) {
+    window.print();
+  }
+});
+
+window.addEventListener('keydown', function(e) {
+  if (e.keyCode == 67) {
+    // assign random ids (c1-9) the circle class
+    // add circleMode bool
+  }
+});
+
+window.addEventListener('keydown', function(e) {
+  if (e.keyCode == 84) {
+    // assign random ids (c1-9) the triangle class
+    // add triangleMode bool
   }
 });
 
 window.addEventListener('keydown', function(e) {
   if (e.keyCode == 82) {
-    generateColors();
-  }
-});
-
-window.addEventListener('keydown', function(e) {
-  if (e.keyCode == 80) {
-    window.print();
-  }
-});
-
-window.addEventListener('keydown', function(e) {
-  if (e.keyCode == 80) {
-    window.print();
+    generateComp();
+    // unassign AND REASSIGN random ids c1-9 the circle and triangle classes IF modes are active
   }
 });
 
